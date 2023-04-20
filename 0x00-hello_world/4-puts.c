986fdd17a2d46361604a022793359b603de5716d
boutaina <tainachan067@gmail.com>
2023-04-20 16:14:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-int main(void)
+
 /**
-*main - Prints quote
-*return- 0
-*/
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
