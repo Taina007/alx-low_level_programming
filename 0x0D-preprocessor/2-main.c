16b6f3d7657c7bd25b4dee9e779778ca315353e7
boutaina <tainachan067@gmail.com>
2023-05-22 14:14:11 -0700

    third task

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	printf("the name of the dile this was compiled from is:%s\n",__FILE__);
-	return 0;
-
+	printf("%s\n", __FILE__);
+	return (0);
 }
